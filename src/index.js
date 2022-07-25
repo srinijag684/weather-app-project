6fcf1fe6a52e3e4257ca2288cc773e234f11ef53
srinija684 <75112894+srinijag684@users.noreply.github.com>
2022-07-25 16:45:25 +0800

    .

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ function displayTemp(response){
   let countryElement = document.querySelector("#Country");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   countryElement.innerHTML = Math.round(response.data.name);
-  console.log("function called");
+  console.log("response.data");
 }
 
 let apiKey = "f6c17ad232aa886321714b7bb48bbe9a";
