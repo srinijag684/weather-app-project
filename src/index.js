b3024e99039a7d2c4d4c0a571229a2365703b9dc
srinija684 <75112894+srinijag684@users.noreply.github.com>
2022-07-25 16:13:16 +0800

    variable edit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 let apiKey = "f6c17ad232aa886321714b7bb48bbe9a";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Japan&appid=${apiKey}&units=metric`;
 
 console.log(apiUrl);
