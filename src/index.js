f331a3ddd326bc0f64c12e81cdd94fd793cece1f
srinija684 <75112894+srinijag684@users.noreply.github.com>
2022-07-25 16:16:37 +0800

    add display temp function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 let apiKey = "f6c17ad232aa886321714b7bb48bbe9a";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Japan&appid=${apiKey}&units=metric`;
 
-console.log(apiUrl);
+function displayTemp(){
+  console.log(Response.data)
+}
+
+axios.get(apiUrl).then(displayTemp);
