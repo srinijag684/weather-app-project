78b07d357a170b4bf97c810a673c0738ecc7afb5
srinija684 <75112894+srinijag684@users.noreply.github.com>
2022-07-25 16:37:30 +0800

    .

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 function displayTemp(response){
-  //console.log(response.data.main.temp);
-
   let temperatureElement = document.querySelector("#temp");
- 
   temperatureElement = response.data.main.temp;
-
 }
 
 let apiKey = "f6c17ad232aa886321714b7bb48bbe9a";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Japan&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?
+q=Japan&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemp);
