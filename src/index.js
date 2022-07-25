4859d09360858ddad7cd556d13d6eaf09066f538
srinija684 <75112894+srinijag684@users.noreply.github.com>
2022-07-25 16:18:01 +0800

    .

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 let apiKey = "f6c17ad232aa886321714b7bb48bbe9a";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Japan&appid=${apiKey}&units=metric`;
 
-function displayTemp(){
+function displayTemp(response){
   console.log(response.data)
 }
 
