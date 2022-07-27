782c2f5d7b510a051566d1a194216c938c01863d
srinija684 <75112894+srinijag684@users.noreply.github.com>
2022-07-27 13:33:10 +0800

    .

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
-function formatDate(timestamp){
-  //calculate the date
+function formatDate(timestamp) {
   let date = new Date(timestamp);
-  let hours = timestamp.getHours();
-  let mins = date.getMinutes();
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
   let day = date.getDay();
-  return `${day} ${hours}:${mins}`;
-  console.log(`${day} ${hours}:${mins}`);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
 }
 
 function displayTemp(response){
