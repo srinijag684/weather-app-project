597ff8f1821aab60ab9203397bcf50efcb397a03
srinija684 <75112894+srinijag684@users.noreply.github.com>
2022-07-27 14:09:47 +0800

    .

--- src/index.js
+++ src/index.js
@@ -52,7 +52,7 @@ function displayTemp(response){
 }
 
 let apiKey = "f6c17ad232aa886321714b7bb48bbe9a";
-let city = "Singapore"
+let city = "Switzerlandx"
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemp);
